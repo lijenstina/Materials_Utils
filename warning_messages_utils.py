788ad80c77bf4a3e84fdd9bd7ce2fc5b2790b9b9
lijenstina <lijenstina@gmail.com>
2016-08-02 23:31:58 +0200

    String.format used instead of adding strings in warning messages
    Refactor of warning_messages_utils

--- warning_messages_utils.py
+++ warning_messages_utils.py
@@ -46,64 +46,64 @@ def warning_messages(operator=None, warn='DEFAULT', object_name="", is_mat=None,
 
                 # compare string list size
                 if (1 < obj_size <= MAX_COUNT):
-                    obj_name = obj_name + gramma_p
+                    obj_name = "{}{}".format(obj_name, gramma_p)
                 elif (obj_size > MAX_COUNT):
                     abbrevation = ("(Multiple)" if is_mat else "(Multiple Objects)")
-                    obj_name = abbrevation + gramma_p
+                    obj_name = "{}{}".format(abbrevation, gramma_p)
                 elif (obj_size == 1):
-                    obj_name = obj_name + gramma_s
+                    obj_name = "{}{}".format(obj_name, gramma_s)
             else:
-                obj_name = object_name + gramma_s
+                obj_name = "{}{}".format(object_name, gramma_s)
 
         message = {
-            'EMPTY': "" + fake,
+            'EMPTY': "".join(fake),
             'DEFAULT': "No editable selected objects, could not finish",
-            'RMV_EDIT': obj_name + "Unable to remove material slot in edit mode",
-            'A_OB_MIX_NO_MAT': obj_name + "No Material applied. Object type can't have materials",
-            'A_MAT_NAME_EDIT': obj_name + " been applied to selection",
-            'C_OB_NO_MAT': obj_name + "No Materials. Unused material slots are "
-            "not cleaned",
-            'C_OB_MIX_NO_MAT': obj_name + "No Materials or an Object type that "
-            "can't have Materials (Clean Material Slots)",
-            'R_OB_NO_MAT': obj_name + "No Materials. Nothing to remove",
-            'R_OB_FAIL_MAT': obj_name + "Failed to remove materials - (Operator Error)",
+            'RMV_EDIT': "{}{}".format(obj_name, "Unable to remove material slot in edit mode)"),
+            'A_OB_MIX_NO_MAT': "{}{}".format(obj_name, "No Material applied. Object type can't have materials"),
+            'A_MAT_NAME_EDIT': "{}{}".format(obj_name, " been applied to selection"),
+            'C_OB_NO_MAT': "{}{}".format(obj_name, "No Materials. Unused material slots are "
+                                         "not cleaned"),
+            'C_OB_MIX_NO_MAT': "{}{}".format(obj_name, "No Materials or an Object type that "
+                                             "can't have Materials (Clean Material Slots)"),
+            'R_OB_NO_MAT': "{}{}".format(obj_name, "No Materials. Nothing to remove"),
+            'R_OB_FAIL_MAT': "{}{}".format(obj_name, "Failed to remove materials - (Operator Error)"),
             'R_NO_SL_MAT': "No Selection. Material slots are not removed",
             'R_ALL_SL_MAT': "All materials removed from selected objects",
             'R_ALL_NO_MAT': "Object(s) have no materials to remove",
-            'R_ACT_MAT': obj_name + "Removed active Material",
-            'R_ACT_MAT_ALL': obj_name + "Removed all Material from the Object",
-            'SL_MAT_BY_NAME': "Objects with the Material " + obj_name + "been selected",
-            'OB_CANT_MAT': obj_name + "Object type that can't have Materials",
+            'R_ACT_MAT': "{}{}".format(obj_name, "Removed active Material"),
+            'R_ACT_MAT_ALL': "{}{}".format(obj_name, "Removed all Material from the Object"),
+            'SL_MAT_BY_NAME': "{}{}{}".format("Objects with the Material ", obj_name, "been selected"),
+            'OB_CANT_MAT': "{}{}".format(obj_name, "Object type that can't have Materials"),
             'REP_MAT_NONE': "Replace Material: No materials replaced",
-            'FAKE_SET_ON': obj_name + "set Fake user " + fake,
-            'FAKE_SET_OFF': obj_name + "disabled Fake user " + fake,
+            'FAKE_SET_ON': "{}{}{}".format(obj_name, "set Fake user ", fake),
+            'FAKE_SET_OFF': "{}{}{}".format(obj_name, "disabled Fake user ", fake),
             'FAKE_NO_MAT': "Fake User Settings: Object(s) with no Materials or no changes needed",
             'CPY_MAT_MIX_OB': "Copy Materials to others: Some of the Object types can't have Materials",
             'CPY_MAT_ONE_OB': "Copy Materials to others: Only one object selected",
             'CPY_MAT_FAIL': "Copy Materials to others: (Operator Error)",
             'CPY_MAT_DONE': "Materials are copied from active to selected objects",
             'TEX_MAT_NO_SL': "Texface to Material: No Selected Objects",
-            'TEX_MAT_NO_CRT': obj_name + "not met the conditions for the tool (UVs, Active Images) ",
+            'TEX_MAT_NO_CRT': "{}{}".format(obj_name, "not met the conditions for the tool (UVs, Active Images)"),
             'MAT_TEX_NO_SL': "Material to Texface: No Selected Objects",
-            'MAT_TEX_NO_MESH': obj_name + "not met the conditions for the tool (Mesh)",
-            'MAT_TEX_NO_MAT': obj_name + "not met the conditions for the tool (Material)",
+            'MAT_TEX_NO_MESH': "{}{}".format(obj_name, "not met the conditions for the tool (Mesh)"),
+            'MAT_TEX_NO_MAT': "{}{}".format(obj_name, "not met the conditions for the tool (Material)"),
             'BI_SW_NODES_OFF': "Switching to Blender Render, Use Nodes disabled",
             'BI_SW_NODES_ON': "Switching to Blender Render, Use Nodes enabled",
             'CYC_SW_NODES_ON': "Switching back to Cycles, Use Nodes enabled",
-            'TEX_RENAME_F': obj_name + "no Images assigned, skipping",
+            'TEX_RENAME_F': "{}{}".format(obj_name, "no Images assigned, skipping"),
             'NO_TEX_RENAME': "No Textures in Data, nothing to rename",
             'DIR_PATH_W_ERROR': "ERROR: Directory without writing privileges",
             'DIR_PATH_N_ERROR': "ERROR: Directory not existing",
             'DIR_PATH_A_ERROR': "ERROR: Directory not accessible",
             'DIR_PATH_W_OK': "Directory has writing privileges",
             'DIR_PATH_CONVERT': "Conversion Cancelled. Problem with chosen Directory, check System Console",
-            'MAT_LINK_ERROR': obj_name + "not be renamed or set as Base(s)",
+            'MAT_LINK_ERROR': "{}{}".format(obj_name, "not be renamed or set as Base(s)"),
             'MAT_LINK_NO_NAME': "No Base name given, No changes applied",
-            'MOVE_SLOT_UP': obj_name + "been moved on top of the stack",
-            'MOVE_SLOT_DOWN': obj_name + "been moved to the bottom of the stack",
-            'MAT_TRNSP_BACK': obj_name + "been set with Alpha connected to Front/Back Geometry node",
+            'MOVE_SLOT_UP': "{}{}".format(obj_name, "been moved on top of the stack"),
+            'MOVE_SLOT_DOWN': "{}{}".format(obj_name, "been moved to the bottom of the stack"),
+            'MAT_TRNSP_BACK': "{}{}".format(obj_name, "been set with Alpha connected to Front/Back Geometry node"),
             'E_MAT_TRNSP_BACK': "Transparent back (BI): Failure to set the action",
-            'CONV_NO_OBJ_MAT': obj_name + "has no Materials. Nothing to convert",
+            'CONV_NO_OBJ_MAT': "{}{}".format(obj_name, "has no Materials. Nothing to convert"),
             'CONV_NO_SC_MAT': "No Materials in the Scene. Nothing to convert",
             'CONV_NO_SEL_MAT': "No Materials on Selected Objects. Nothing to convert",
             }
